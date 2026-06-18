d56e75bbb7ea6ea1dd166bc41f64f5b3fa3b406c
Amit4218 <amitbhagat621@gmail.com>
2026-06-18 15:04:29 +0530

    added application settings

--- settings.py
+++ settings.py
@@ -1,14 +1,17 @@
-from pathlib import Path
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import BaseModel
+from typing import Dict
 
 
-class Settings(BaseSettings):
-    BASE_URL: str
-    RESOURCE_PAGE: str
-    RESOURCE_ID: str
-    RESOURCE_ORIGIN: str
-    DOWNLOAD_DIR:Path = Path("temp")
-
-    model_config = SettingsConfigDict(env_file=".env")
+class ApplicationSettings(BaseModel):
+    title: str = "Pinsearch"
+    summary: str = "Search and verify 6-digit Indian PIN codes instantly."
+    description: str = "Get accurate address information, including State, District, Region, and geographic coordinates directly from official datasets."
+    version: str = "1.0.0"
+    contact: Dict[str, str] = {
+        "name": "Amit Bhagat",
+        "email": "amitbhagat621@gmail.com",
+    }
+    docs_url: str | None = None
+    redoc_url: str | None = None
+    openapi_url: str | None = None
     
-settings = Settings() # type: ignore
